f4b115a67fd041dee25207f15b39fca9983e3d29
szaroket <mpiestrzynska@interia.pl>
2017-08-24 14:45:24 +0200

    Add big letter after clicked CapsLock and Shift

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 // JavaScript source code
 $(document).ready(function () {
-    $('button').on('click',function () {
-        var letter = $(this).text();
-        var capslock = false;
+    //after page refresh - textarea is empty
+    $('#text1, input[type=text]').val('')
+    var textBefore = $('#text1');
+    var capslock = false;
+    var shift = false;
+    var i;
 
-        if (capslock = false) {
-            //write small letter from clicked button
-            $('textarea')[0].value += letter;
-        } else {
-            //write big letter from clicked button
-            $('textarea')[0].value += letter.toUpperCase();
+    $('button').click(function () {
+        var letter = $(this).text();
+        
+        if (this.id == "return") {
+            letter = '\n';
         }
+        if (this.id == "tab") {
+            letter = '\t';
+        }
+        if (this.id == 'shiftL' || this.id == 'shiftR') {
+            for (i = 1; i < 26; i++) {
+                $("#letter" + i).attr("id", "uppercase");
+            }
+            shift = (shift === true) ? false : true;
+            capslock = false;
+            return false;
+        }
+        if (this.id == 'caps') {
+            for (i = 1; i < 26; i++) {
+                $("#letter" + i).attr("id", "uppercase");
+            }
+            capslock = true;
+            return false;
+        }
+        if (this.id == "uppercase") {
+            letter = letter.toUpperCase();
+        }
+
+        //write letter from clicked button
+        textBefore[0].value += letter;
         
         //clear the textarea
         if (this.id == "clear") {
-            $('textarea')[0].value = null;
-        }
-        if (this.id == "caps") {
-            capslock = true;
-            $('textarea')[0].value = 0;
-            return false;
+            $('textarea, input[type=text]').val('')
         }
     });
-
-    //after page refresh - textarea is empty
-    $('textarea, input[type=text]').val('')
 });
