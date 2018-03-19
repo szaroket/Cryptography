6d943e455acea5ebc4f474e20592428e56f47f28
szaroket <mpiestrzynska@interia.pl>
2018-03-19 14:52:40 +0100

    Change the style of variable declaration

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,30 +1,32 @@
 //Caesar Cipher
 function caesarCipher() {
-    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z', 'a', 'b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z', '0','1','2','3','4','5','6','7','8','9'];
-    var marks = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', "'", '"', '\\', '|','<','>','?',',','.','/'];
-    var shift = parseInt(prompt('Enter the value of shift'));
-    var str = document.getElementById("text1").value;
-    var newStr = null;
-    var k;
-    var output = '';
-    var sign;
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z', 'a', 'b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z', '0','1','2','3','4','5','6','7','8','9'],
+        marks = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', "'", '"', '\\', '|','<','>','?',',','.','/'],
+        shift = parseInt(prompt('Enter the value of shift')),
+        str = document.getElementById("text1").value,
+        newStr = null,
+        k = 0,
+        z = 0,
+        c = '',
+        output = '',
+        sign = '';
 
     for (k = 0; k < (str.length); k++) {
-        var c = str[k];
+        c = str[k];
 
         if (shift < 62) {
             if (alphabet.indexOf(c) != -1) {
-                var sign = parseInt(alphabet.indexOf(c));
-                var z = sign + shift;
+                sign = parseInt(alphabet.indexOf(c));
+                z = sign + shift;
                 newStr = alphabet[z];
             } else {
                 newStr = c;
             }
-            
+
         }
 
         output += newStr;
     }
 
-    document.getElementById("text2").innerHTML = output; 
+    document.getElementById("text2").innerHTML = output;
 }
